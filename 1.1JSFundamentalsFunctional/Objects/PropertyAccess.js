1bf7f252757b7b03cbc2dacc3f00da5964ea1e9b
Alan Torres <alan9805@gmail.com>
2021-09-05 20:55:14 -0500

    Introduction

---
+++ 1.1JSFundamentalsFunctional/Objects/PropertyAccess.js
@@ -0,0 +1,12 @@
+// Assigments with dots
+let person = {};
+person.name = "Mrs. Ibarra";
+
+// Access with dots
+let who = person.name;
+who; // "Mrs. Ibarra"
+
+person.name = "Mr. Ibarra";
+who; // "Mrs. Ibarra"
+
+who.story; // undefined
