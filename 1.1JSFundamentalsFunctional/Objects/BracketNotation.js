c9462c0511cbecc1d6e1951a226f953890651cb6
Alan Torres <alan9805@gmail.com>
2021-09-05 20:55:36 -0500

    Bracket notation

---
+++ 1.1JSFundamentalsFunctional/Objects/BracketNotation.js
@@ -0,0 +1,10 @@
+let person = [];
+let plea = "wouldShe";
+
+person.name = "Mrs. Ibarra"; // "name": "Mrs. Ibarra"
+
+person[plea] = "I would never!"; // "wouldShe": "I would never!"
+
+person["plea"] = "I would never"; // "plea": "I would never"
+
+person.plea; // I would never"
