4eae3bebab984f83d80ef75e9944c333b86c1683
Alan Torres <alan9805@gmail.com>
2021-09-05 21:55:16 -0500

    Non-Valid characters

---
+++ 1.1JSFundamentalsFunctional/Objects/Non-validCharacters.js
@@ -0,0 +1,15 @@
+let box = {};
+
+box['material'] = "cardboard";
+box[0] = 'meow';
+box['^&*'] = "testing 123";
+box['Alan-torres'] = "name";
+
+let test = box['^&*'];
+
+// box = {
+//     0: "meow",
+//     "^&*": "testing 123",
+//     "material": "cardboard",
+//     "Alan-torres": "name"
+// }
