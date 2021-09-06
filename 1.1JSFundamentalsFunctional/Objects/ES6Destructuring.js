f90872189af0113dc04fd35d9068e4b94c7b1107
Alan Torres <alan9805@gmail.com>
2021-09-05 22:36:06 -0500

    ES6 destructuring

---
+++ 1.1JSFundamentalsFunctional/Objects/ES6Destructuring.js
@@ -0,0 +1,18 @@
+// Destructuring is a simplified way to define variables and taking them outside of an array or object
+
+// Arrays
+// Variable declarations:
+const [first, second] = [true, false];
+let [first, second] = [true, false];
+
+// Assigment:
+[first, second] = [true, false];
+
+
+// Objects
+// Variable declarations:
+const { first, second } = { first: 0, second: 1 };
+let { first, second } = { first: 0, second: 1 };
+
+// Assigment:
+// { first, second } = { first: 0, second: 1 };
