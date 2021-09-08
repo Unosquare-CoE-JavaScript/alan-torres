40b083868bd347d05a10ec34e06f9ee18522fc3a
Alan Torres <alan9805@gmail.com>
2021-09-07 20:34:14 -0500

    Coercion exercise

---
+++ 1.2DeepJavascriptFoundations/Coercion/CoercionExercise.js
@@ -0,0 +1,23 @@
+function isValidName(name) {
+    return typeof name === "string" && name.trim().length >= 3
+}
+
+function hoursAttended(attended, length) {
+    if (typeof attended === "string"  && attended.trim() != "") {
+        attended = Number(attended);
+    }
+
+    if (typeof length === "string"  && length.trim() != "") {
+        length = Number(length);
+    }
+
+    if (
+        typeof attended === "number" && typeof length === "number" &&
+        attended >= 0 && length >= 0 && 
+        Number.isInteger(attended) && Number.isInteger(length) &&
+        attended <= length
+    ) {
+        return true;
+    }
+    return false;
+}
