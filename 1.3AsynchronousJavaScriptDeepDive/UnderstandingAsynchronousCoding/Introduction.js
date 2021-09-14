dc66effed981fc1cd5f1738c0ad2cfef8c996d83
Alan Torres <alan9805@gmail.com>
2021-09-14 15:05:20 -0500

    Understanding Asynchronous Coding section

---
+++ 1.3AsynchronousJavaScriptDeepDive/UnderstandingAsynchronousCoding/Introduction.js
@@ -0,0 +1,19 @@
+
+// Synchronous ex.
+
+"use strict";
+
+const test = function() {
+    console.log("Start of code");
+
+    alert("Notice Me!");
+
+    console.log("End of code!");
+};
+
+const test2 = function() {
+    console.log("Now I get attention.");
+};
+
+test();
+test2();
---
+++ 1.3AsynchronousJavaScriptDeepDive/UnderstandingAsynchronousCoding/UnderstandingTheEventLoop.js
@@ -0,0 +1,9 @@
+
+// The Event Loop * * * * *
+
+// Call Stack: Function calls form a stack of frames.
+// Heap: Objects are allocated in a heap which is just a name to denote a large (mostly unstructured)
+    // region of memory.
+// Queue: A JavaScript runtime uses a message queue, which is a list of messages to be processed.
+    // Each message has an associated function which gets called in order to handle the message.
+
