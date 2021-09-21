c56a3ccf44426a4ac00af91938035035ffc0c166
Alan Torres <alan9805@gmail.com>
2021-09-21 17:51:05 -0500

    Adapter section

---
+++ 1.4DesignPatternsInJavaScript/6_Adapter/AdapterCodingExercise.js
@@ -0,0 +1,24 @@
+
+class Square {
+    constructor(side) {
+        this.side = side;
+    }
+}
+
+function area(rectangle) {
+    return rectangle.width * rectangle.height;
+}
+
+class SquareToRectangleAdapter {
+    constructor(square) {
+        this.square = square;
+    }
+
+    get width() {
+        return this.square.side;
+    }
+
+    get height() {
+        return this.square.side;
+    }
+}
