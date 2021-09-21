4b5b95f2e90ce36770f8dd00270e99385ba13727
Alan Torres <alan9805@gmail.com>
2021-09-21 17:51:26 -0500

    Bridge section

---
+++ 1.4DesignPatternsInJavaScript/7_Bridge/BrideCodingExercise.js
@@ -0,0 +1,35 @@
+
+class Shape {
+    constructor(renderer, name = null) {
+        this.renderer = renderer;
+        this.name = name;
+    }
+
+    toString() {
+        return `Drawing ${this.name} as ${this.renderer.whatToRenderAs}`;
+    }
+}
+
+class Triangle extends Shape {
+    constructor(renderer) {
+        super(renderer, "triangle");
+    }
+}
+
+class Square extends Shape {
+    constructor(renderer) {
+        super(renderer, "square");
+    }
+}
+
+class RasterRenderer {
+    get whatToRenderAs() {
+        return "pixels";
+    }
+}
+
+class VectorRenderer {
+    get whatToRenderAs() {
+        return "lines";
+    }
+}
