18c0544caa6a76c3d1d9af8398730820c475609d
Alan Torres <alan9805@gmail.com>
2021-09-21 17:56:39 -0500

    State section

---
+++ 1.4DesignPatternsInJavaScript/20_State/Overview.js
@@ -0,0 +1,12 @@
+
+// * State *
+
+// State: A pattern in which the object's behavior is determined by its state. An object transitions from one state to
+// another (something needs to trigger a transition).
+// A formalized construct which manages state and transitions is called a state machine.
+// - Given sufficient complexity, it pays to formally define possible states and events/triggers
+// - Can define
+//      - State entry/exit behaviors
+//      - Action whrn a particular event causes a transition
+//      - Guard conditions enabling/disabling a transition
+//      - Default action when no transitions are found for an event
