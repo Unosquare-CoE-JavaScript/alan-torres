08fc85ad2ad118915ecb84743e6954f0ca19dd70
Alan Torres <alan9805@gmail.com>
2021-10-25 10:39:35 -0500

    References

---
+++ 1.6ReactTesting/references.js
@@ -0,0 +1,12 @@
+/*
+
+https://www.w3.org/TR/wai-aria/#role_definitions
+https://testing-library.com/docs/queries/about/#priority
+https://github.com/testing-library/jest-dom
+
+https://github.com/testing-library/eslint-plugin-testing-library
+https://github.com/testing-library/eslint-plugin-jest-dom
+
+https://mswjs.io/
+
+*/
